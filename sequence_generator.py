5c3d59b8f99195c5376aeb8e7ccd9736a620c971
marmaduke woodman <marmaduke.woodman@univ-amu.fr>
2025-04-04 18:39:11 +0200

    seq

--- sequence_generator.py
+++ sequence_generator.py
@@ -4,21 +4,12 @@ def generate_sequence(length):
     """Generate a sequence of random length (between 2 and 5) random numbers between 0 and 9, repeated until the desired length."""
     base_length = np.random.randint(2, 6)
     base_sequence = np.random.randint(0, 10, size=base_length)
-    full_sequence = np.tile(base_sequence, (length // 4 + 1))[:length]
+    full_sequence = np.tile(base_sequence, (length // base_sequence.size + 1))[:length]
     return full_sequence
 
 def generate_batch(batch_size, num_tokens):
     """Generate a batch of sequences with fixed length."""
-    batch = []
-    for _ in range(batch_size):
-        seq = generate_sequence(num_tokens)
-        if len(seq) < num_tokens:
-            seq = np.pad(seq, (0, num_tokens - len(seq)), 'constant')
-        else:
-            seq = seq[:num_tokens]
-        batch.append(seq)
-    return np.array(batch)
-    return np.array(batch)
+    return np.array([generate_sequence(num_tokens) for _ in range(batch_size)])
 
 # Example usage
 if __name__ == "__main__":
